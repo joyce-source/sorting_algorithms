6bcc1e89500ddc44461b8fcc6ee0bbc44de4e625
joyce-source <brownjojo97@gmail.com>
2023-04-25 05:03:33 -0700

    task_error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,24 +8,22 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int i, j, temp;
+	unsigned int i, j;
+	int tmp;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j =0; j < size - i - 1; j++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[j] > array[j +1])
 			{
-				temp = array[j];
+				/*swap adjecent elements*/
+				tmp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				array[j + 1] = tmp;
+
+				print_array(array, size);
 			}
 		}
-		/*print the array after each iteration*/
-		for (int k = 0; k < size; k++)
-		{
-			printf("%d ", array[k]);
-		}
-		printf("\n");
 	}
 }
