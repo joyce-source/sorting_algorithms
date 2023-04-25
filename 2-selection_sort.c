24f5d7666712c4929e98804d9f63e866c6444202
joyce-source <brownjojo97@gmail.com>
2023-04-25 06:29:59 -0700

    selection_sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: the array to sort
+ * @size: the size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+			{
+				min_idx = j;
+			}
+		}
+		if (min_idx != i)
+		{
+			int tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
+	}
+}
